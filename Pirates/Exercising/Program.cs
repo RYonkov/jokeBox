e3d1926b39fc805f4eae41ec62ec4d0ad0144180
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-03-24 11:40:27 +0200

    Solution for the problem P!rates

---
+++ Pirates/Exercising/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercising
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Radoslav";
+            word = string.Join("", word.Reverse());
+            Console.WriteLine(word);
+
+            char[] newPass = word.Where((symbol, index) => index%2!=0).ToArray();
+            word = new string(newPass);
+            Console.WriteLine(word);
+            Console.WriteLine(String.Join("", newPass));
+        }
+    }
+}

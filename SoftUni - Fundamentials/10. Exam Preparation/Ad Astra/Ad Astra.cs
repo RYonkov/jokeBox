525b23eaedc5e3f70a624294dc52ee1b509d86f6
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-03-25 08:15:38 +0200

    Added Exercises
    The Imitation Game
    Ad Astra

---
+++ SoftUni - Fundamentials/10. Exam Preparation/Ad Astra/Ad Astra.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+using System.Linq;                          //For lambda expressions
+using System.Collections.Generic;           //For Dictionaries
+using System.Numerics;                      //For BigInteger
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+
+
+namespace ExamPreparation
+{
+    internal class Program
+    {
+        class Product
+        {
+            public Product(string name, string date, int calories)
+            {
+                this.Name = name;
+                this.ExpirationDate = date;
+                this.Calories = calories;
+            }
+            public string Name { get; set; }
+            public string ExpirationDate { get; set; }
+            public int Calories { get; set; }
+        }
+        
+        static void Main(string[] args)
+        {
+            string seekPattern = @"([#|\|])([A-Za-zА-Яа-я\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d{1,5})\1";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, seekPattern);
+            List<Product> products = new List<Product>();
+            int totalSum = 0;
+
+            foreach (Match match in matches)
+            {
+                string product = match.Groups[2].Value;
+                string date = match.Groups[3].Value;
+                int calories = int.Parse(match.Groups[4].Value);
+                Product current = new Product(product, date, calories);
+                products.Add(current);
+                totalSum += calories;
+            }
+            
+
+            Console.WriteLine($"You have food to last you for: {totalSum/2000} days!");
+            foreach (Product element in products)
+            {
+                Console.WriteLine($"Item: {element.Name}, Best before: {element.ExpirationDate}, Nutrition: {element.Calories}");
+            }
+
+        }
+    }
+}
---
+++ SoftUni - Fundamentials/10. Exam Preparation/ExamPreparation/The Imitation Game.cs
@@ -0,0 +1,52 @@
+﻿using System; 
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+using System.Linq;                          //For lambda expressions
+using System.Collections.Generic;           //For Dictionaries
+using System.Numerics;                      //For BigInteger
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+
+
+namespace ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (command!="Decode")
+            {
+                string[] cmdArgs = command.Split('|', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string action = cmdArgs[0];
+
+                if (action== "Move")
+                {
+                    int numberOfLetters = int.Parse(cmdArgs[1]);
+                    string current = message.Substring(0, numberOfLetters);
+                    message = message.Remove(0, numberOfLetters);
+                    message = message + current;                    
+                }
+                else if (action== "Insert")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string value = cmdArgs[2];
+                    message = message.Insert(index, value);
+                }
+                else if (action== "ChangeAll")
+                {
+                    string substring = cmdArgs[1];
+                    string replacement = cmdArgs[2];
+                    message = message.Replace(substring, replacement);
+                }
+                
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}

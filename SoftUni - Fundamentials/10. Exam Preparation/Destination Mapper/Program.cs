c349031d640b85f141f1c9b6f3085fa04ccf46f5
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-03-26 12:37:15 +0200

    Added Destination Mapper

---
+++ SoftUni - Fundamentials/10. Exam Preparation/Destination Mapper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+using System.Linq;                          //For lambda expressions
+using System.Collections.Generic;           //For Dictionaries
+using System.Numerics;                      //For BigInteger
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+
+
+namespace ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string seekPattern = @"([=\/])([A-Z][a-z]{2,})\1";
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, seekPattern);
+            List<string> destinations = new List<string>();
+            int totalSum = 0;
+          
+            foreach (Match match in matches)
+            {
+                string destination = match.Groups[2].Value;    
+                destinations.Add(destination);
+                totalSum += destination.Length;
+            }                    
+            Console.WriteLine($"Destinations: {String.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {totalSum}");
+        }
+    }
+}

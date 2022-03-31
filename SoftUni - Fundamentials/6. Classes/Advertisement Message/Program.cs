00a0e2ff062ad6258c80b9c1c936db9a844b63f1
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-03-31 09:08:49 +0300

    Added Advertisement Message

---
+++ SoftUni - Fundamentials/6. Classes/Advertisement Message/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+using System.Linq;                          //For lambda expressions
+using System.Collections.Generic;           //For Dictionaries
+using System.Numerics;                      //For BigInteger
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+
+
+namespace RadoExerciseClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> phrases = new List<string> { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            List<string> events = new List<string> { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            List<string> authors = new List<string> { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            List<string> cities = new List<string> { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Random rnd = new Random();
+                int randomPhraseIndex = rnd.Next(0, phrases.Count);
+                int randomEventIndex = rnd.Next(0, events.Count);
+                int randomAuthorsIndex = rnd.Next(0, authors.Count);
+                int randomCitiesIndex = rnd.Next(0, cities.Count);
+                Console.WriteLine($"{phrases[randomPhraseIndex]} {events[randomEventIndex]} {authors[randomAuthorsIndex]} - {cities[randomCitiesIndex]}");
+            }
+
+        }
+    }
+}

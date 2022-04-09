086b771b1363a5d904751083f60f18a60c3e1ded
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-09 11:36:54 +0300

    Added Append Arrays

---
+++ SoftUni - Fundamentials/7.Lists/AppendArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrays = Console.ReadLine()
+                                     .Split('|')
+                                     .ToArray();
+
+            List<int> output = new List<int>();
+
+            for (int i = arrays.Length - 1; i > -1; i--)
+            {
+                int[] currentArray = arrays[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                output.AddRange(currentArray);                
+            }
+            Console.WriteLine(String.Join(" ", output));           
+        }
+    }
+}

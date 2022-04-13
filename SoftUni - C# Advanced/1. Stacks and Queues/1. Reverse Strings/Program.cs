3aa733222b59b81b64d3f86cdd900aea6c727483
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-13 08:13:22 +0300

    Added Reverse Strings (C# Advanced)

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+
+namespace ReverseStrings
+{
+	class Program
+	{
+		static void Main()
+		{
+			string input = Console.ReadLine();
+			Stack<char> stack = new Stack<char>(input);				
+			
+					
+			while (stack.Count > 0)
+			{
+				Console.Write(stack.Pop());
+			}
+		}
+        
+    }
+}

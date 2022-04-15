11494f4e26df1b9f8b1d437f1a73d1c895108128
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-15 07:14:00 +0300

    Added 3. Simple Calculator

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/3. Simple Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _3._Simple_Calculator
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			string[] input = Console.ReadLine().Split (' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+			Queue<string> queue = new Queue<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+				queue.Enqueue(input[i]);
+            }
+					
+			int output = int.Parse(queue.Dequeue()); 
+			while (queue.Count > 0)
+            {
+                char ch = queue.Dequeue()[0];
+				int next = int.Parse(queue.Dequeue());
+				
+				if (ch =='+')
+                {
+					output += next;
+                }
+                else if (ch=='-')
+                {
+					output -= next;
+                }
+            }
+			Console.WriteLine(output);
+		}
+	}
+}

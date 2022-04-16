8fde1587031f03e29fa4a4956659573f063407bc
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-16 12:53:22 +0300

    Added 4. Matching Brackets

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/4. Matching Brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _4._Matching_Brackets
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			string input = Console.ReadLine();
+			Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {				          
+				if (input[i] =='(')
+                {
+					stack.Push(i);
+                }
+				else if (input[i] ==')')
+                {
+                    int startIndex = stack.Pop();
+					int endIndex = i;
+					Console.WriteLine(input.Substring(startIndex, endIndex-startIndex+1));
+                }
+            }
+		}
+	}
+}

0933746447662ddf902e461b9f1cb113177b510c
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-18 07:46:17 +0300

    Added 6. Supermarket

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/6. Supermarket/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace Supermarket
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			Queue<string> supermarketQueue = new Queue<string>();
+			string input = Console.ReadLine();
+			while (input != "End")
+			{
+				if (input == "Paid")
+				{
+					foreach (string element in supermarketQueue)
+					{
+						Console.WriteLine(element);
+					}
+					supermarketQueue.Clear();
+				}
+				else
+				{
+					supermarketQueue.Enqueue(input);
+				}
+				input = Console.ReadLine();
+			}
+			Console.WriteLine($"{supermarketQueue.Count} people remaining.");
+		}
+	}
+}

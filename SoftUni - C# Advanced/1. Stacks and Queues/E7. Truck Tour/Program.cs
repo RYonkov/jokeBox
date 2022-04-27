fd853f1d267e24a7361803e1f0684851fe567e74
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-27 07:34:20 +0300

    Added E7. Truck Tour

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/E7. Truck Tour/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E7.TruckTour
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			List<int> fuelStations = new List<int>();
+
+
+			for (int i = 0; i < n; i++)
+			{
+				int[] station = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+				int current = station[0] - station[1];
+				fuelStations.Add(current);
+			}
+
+			Queue<int> queue = new Queue<int>(fuelStations);
+
+			for (int j = 0; j < n; j++)
+			{
+				bool hasNotFailed = true;
+				long totalFuel = 0;
+
+				foreach (int element in queue)
+				{
+					totalFuel += element;
+					if (totalFuel < 0)
+					{
+						hasNotFailed = false;
+						break;
+					}
+				}
+
+				if (hasNotFailed)
+				{
+					Console.WriteLine(j);
+					break;
+				}
+				else
+				{
+					queue.Enqueue(queue.Dequeue());
+				}
+			}
+		}
+	}
+}

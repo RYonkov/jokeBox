a7e56dc575574b28ef7519ab4426f790b739e169
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-04-28 07:28:21 +0300

    Added E8. Balanced Parenthesis

---
+++ SoftUni - C# Advanced/1. Stacks and Queues/E8. Balanced Parenthesis/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E8.BalancedParenthesis
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] brackets = Console.ReadLine().ToArray();
+			Stack<char> stack = new Stack<char>();
+
+			foreach (var element in brackets)
+			{
+				if (element == '(' || element == '[' || element == '{')
+				{
+					stack.Push(element);
+					continue;
+				}
+
+				if ((element == ')' && stack.Count > 0 && stack.Peek() == '(') || (element == ']' && stack.Count > 0 && stack.Peek() == '[') || (element == '}' && stack.Count > 0 && stack.Peek() == '{'))
+				{
+					stack.Pop();
+					continue;
+				}
+
+				if (element != '(' || element != '[' || element != '{' || element != ')' || element != ']' || element != '}')
+				{
+					stack.Push(element);
+					break;
+				}
+			}
+
+			if (stack.Count == 0)
+			{
+				Console.WriteLine("YES");
+			}
+			else
+			{
+				Console.WriteLine("NO");
+			}
+		}
+	}
+}

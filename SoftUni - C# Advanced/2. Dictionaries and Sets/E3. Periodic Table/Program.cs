16a3ca4ba12b55ad5770207e1fff3187b54bdb2c
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-03 07:32:59 +0300

    Added E3. Periodic Table

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/E3. Periodic Table/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E3.PeriodicTable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+
+			HashSet<string> elements = new HashSet<string>();
+
+
+			for (int i = 0; i < n; i++)
+			{
+				string[] input = Console.ReadLine().Split(' ').ToArray();
+				for (int j = 0; j < input.Length; j++)
+				{
+					elements.Add(input[j]);
+				}
+			}
+			elements = elements.OrderBy(x => x).ToHashSet();
+
+			Console.Write(String.Join(' ', elements));
+		}
+	}
+}

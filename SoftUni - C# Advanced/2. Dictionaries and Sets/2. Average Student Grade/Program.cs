7641a33db0d506dc5bb218f392337127356af0fc
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-08 00:07:42 +0300

    Added 2. Average Student Grade

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/2. Average Student Grade/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues, sets
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _2.Average_Student_Grade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> studentGrades  = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (!studentGrades.ContainsKey(input[0]))
+                {
+                    studentGrades.Add(input[0], new List<decimal>());
+                }
+                studentGrades[input[0]].Add(decimal.Parse(input[1]));                             
+            }
+
+            foreach (var element in studentGrades)
+            {
+                string grades = String.Join(' ', element.Value);
+                Console.Write($"{element.Key} -> ");
+                foreach (var grade in element.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }                
+                Console.WriteLine($"(avg: {element.Value.Average():F2})");
+            }
+        }
+    }
+}

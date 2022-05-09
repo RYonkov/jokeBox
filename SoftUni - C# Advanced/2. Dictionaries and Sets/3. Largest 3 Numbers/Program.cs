e5b243a36eeee020119a9a0fd36d0e18be08fad4
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-09 07:44:10 +0300

    Added 3. Largest 3 Numbers

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/3. Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues, sets
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _3.Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(String.Join(' ', Console.ReadLine().Split(' ').Select(int.Parse).ToArray().OrderByDescending(x => x).Take(3)));
+        }
+    }
+}

6f6a23ff057d0d080938198ed2f8e05ad21b1766
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-10 08:25:33 +0300

    Added 3. Product Shop

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/4. Product Shop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues, sets
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _4.Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, double>> shops = new Dictionary<string, Dictionary<string, double>>();
+            while (input!= "Revision")
+            {
+                //"{shop}, {product}, {price}".  
+                string[] cmdArgs = input.Split(", ");
+                string shop = cmdArgs[0];
+                string product = cmdArgs[1];
+                double price = double.Parse(cmdArgs[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    shops.Add(shop, new Dictionary<string, double>());
+                }
+                shops[shop].Add(product, price);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var element in shops.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{element.Key}->");
+
+                foreach (var product in element.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+
+
+        }
+    }
+}

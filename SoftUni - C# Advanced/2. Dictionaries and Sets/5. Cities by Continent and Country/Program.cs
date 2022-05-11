5583d8d677c0fc2315e434f73f95ca8eba395f91
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-11 07:41:14 +0300

    Added Cities by Continent and Country

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/5. Cities by Continent and Country/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues, sets
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _5.Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> places = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!places.ContainsKey(continent))
+                {
+                    places.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!places[continent].ContainsKey(country))
+                {
+                    places[continent].Add(country, new List<string>());
+                }                
+                places[continent][country].Add(city);                
+            }
+            
+            
+            
+
+            foreach (var element in places)
+            {
+                Console.WriteLine($"{element.Key}:");
+
+                foreach (var country in element.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {String.Join(", ", country.Value)}");
+                }
+            }
+
+
+        }
+    }
+}

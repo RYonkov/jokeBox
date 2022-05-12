0c9803dbc120f7d0576ac0c3837b99cadc1afa6a
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-12 07:25:43 +0300

    Added 6. Record Unique Names

---
+++ SoftUni - C# Advanced/2. Dictionaries and Sets/6. Record Unique Names/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues, sets
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _6.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                set.Add(input);
+            }
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}

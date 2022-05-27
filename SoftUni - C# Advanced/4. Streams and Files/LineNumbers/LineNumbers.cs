3fcd6186c3e66b539f30b513ff4339c5c5156b2d
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-28 00:04:29 +0300

    Added 2. Line Numbers

---
+++ SoftUni - C# Advanced/4. Streams and Files/LineNumbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 1;
+                string line;
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                    }                    
+                }
+            }
+        }
+    }
+}

fd06ab0b2043123eee8c92415aa0b0cc8db7ff6f
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-05-30 07:38:01 +0300

    Added 3. Merge Files

---
+++ SoftUni - C# Advanced/4. Streams and Files/MergeFiles/MergeFiles.cs
@@ -0,0 +1,49 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main(string[] args)
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            using (StreamReader first = new StreamReader(firstInputFilePath))
+            {
+                using (StreamReader second = new StreamReader(secondInputFilePath))
+                {
+                    using (StreamWriter output = new StreamWriter(outputFilePath))
+                    {
+                        string line;
+                        string line1="";
+
+                        while (true)
+                        {
+                            if ((line = first.ReadLine())!=null)
+                            {
+                                output.WriteLine(line);
+                            }
+                            if ((line1 = second.ReadLine()) != null)
+                            {
+                                output.WriteLine(line1);
+                            }
+
+                            if (line==null && line1==null)
+                            {
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

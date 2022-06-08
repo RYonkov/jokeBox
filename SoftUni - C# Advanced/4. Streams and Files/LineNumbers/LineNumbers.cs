d1238cddf352231411c08cb3cbfed923035e1042
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-06-08 08:12:53 +0300

    Added Line Numbers

--- SoftUni - C# Advanced/4. Streams and Files/LineNumbers/LineNumbers.cs
+++ SoftUni - C# Advanced/4. Streams and Files/LineNumbers/LineNumbers.cs
@@ -1,30 +1,53 @@
 ﻿namespace LineNumbers
 {
-    using System.IO;
+    using System;
+    using System.Collections.Generic;           //For Dictionaries, stacks and queues
+    using System.Diagnostics;                   //For clock, stopwatch
+    using System.Globalization;                 //For regional specifics
+    using System.IO;                            //For IO operations; 
+    using System.Linq;                          //For lambda expressions
+    using System.Numerics;                      //For BigInteger
+    using System.Text;                          //For StringBuilder
+    using System.Text.RegularExpressions;       //For Regex 
     public class LineNumbers
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string inputPath = @"..\..\..\Files\input.txt";
-            string outputPath = @"..\..\..\Files\output.txt";
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
 
-            RewriteFileWithLineNumbers(inputPath, outputPath);
+            ProcessLines(inputFilePath, outputFilePath);
         }
 
-        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
         {
-            StreamReader reader = new StreamReader(inputFilePath);
-            using (reader)
+            using (StreamReader reader = new StreamReader(inputFilePath))
             {
-                int counter = 1;
-                string line;
                 using (StreamWriter writer = new StreamWriter(outputFilePath))
                 {
-                    while ((line = reader.ReadLine()) != null)
+                    int counter = 1; 
+                    string line = reader.ReadLine();
+                    while (line!=null)
                     {
-                        writer.WriteLine($"{counter}. {line}");
+
+                        int counterLetter = line.Count(Char.IsLetter);
+                        int counterSigns = line.Count(Char.IsPunctuation);
+
+                        //for (int i = 0; i < line.Length; i++)
+                        //{
+                        //    if (Char.IsLetter(line[i]))
+                        //    {
+                        //        counterLetter++;
+                        //    }
+                        //    if (Char.IsPunctuation(line[i]))
+                        //    {
+                        //        counterSigns++;
+                        //    }
+                        //}
+                        writer.WriteLine($"Line {counter}: {line} ({counterLetter})({counterSigns})");
                         counter++;
-                    }                    
+                        line = reader.ReadLine();
+                    }
                 }
             }
         }

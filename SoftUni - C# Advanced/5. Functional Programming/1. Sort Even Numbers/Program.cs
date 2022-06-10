f6484556cef04375dd4d467cb6e6caaed723d0ff
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-06-10 07:45:17 +0300

    Added 5.FP - 1. Sort Even Numbers

---
+++ SoftUni - C# Advanced/5. Functional Programming/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _1._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] output = input.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(String.Join(", ", output));
+        }
+    }
+}

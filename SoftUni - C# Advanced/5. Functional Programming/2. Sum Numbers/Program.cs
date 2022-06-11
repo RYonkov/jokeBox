a81a5a5f63490f31f14fc036540fe064bab97ca0
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-06-11 10:18:50 +0300

    Added 5.FP - 2. Sum Numbers

---
+++ SoftUni - C# Advanced/5. Functional Programming/2. Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _2._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                
+            string input = Console.ReadLine();
+            Func<string, int> parsing = x => int.Parse(x);
+            Func<string, int> parsing2 = (x) => { return int.Parse(x); };
+            int[] numbers = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(parsing2).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}

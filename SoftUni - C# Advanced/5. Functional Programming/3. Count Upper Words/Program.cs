07c820c5a12309164ec599c2685e9130099f1ba0
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-06-12 11:39:36 +0300

    Added 5.FP - 3. Count Upper Words

---
+++ SoftUni - C# Advanced/5. Functional Programming/3. Count Upper Words/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace _3._Count_Upper_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> predicate = x =>Char.IsUpper(x[0]);
+            string[] output = input.Where(predicate).ToArray();
+
+            foreach (string s in output)
+            {
+                Console.WriteLine(s);
+            }
+
+            
+        }
+    }
+}

2bc93a53b0c4f4473e50255175ef353bb6c74390
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-06-18 12:40:04 +0300

    Added 5.FP - E3. Custom Min Function

---
+++ SoftUni - C# Advanced/5. Functional Programming/E3. Custom Min Function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E3._CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();                       
+            Func<List<int>, int> takeMin = delegate (List<int> x) { return x.Min(); };
+            Console.WriteLine(takeMin(nums));
+
+            //Func<List<int>, int> takeMax = x => x.Max();                 
+            //Console.WriteLine(takeMax(nums));
+        }
+    }
+}

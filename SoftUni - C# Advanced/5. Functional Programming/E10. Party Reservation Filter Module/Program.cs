261454ca060b5cec2f10f1aa087c62402b3e7caf
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-02 00:02:58 +0300

    Added E10. Party Reservation Filter Mode

---
+++ SoftUni - C# Advanced/5. Functional Programming/E10. Party Reservation Filter Module/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E10._Party_Reservation_Filter_Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

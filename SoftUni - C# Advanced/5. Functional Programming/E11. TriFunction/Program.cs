042d636729ba3b2e5df03ce6714840be46eedaff
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-03 10:13:58 +0300

    Added E11. TriFunction

---
+++ SoftUni - C# Advanced/5. Functional Programming/E11. TriFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;           //For Dictionaries, stacks and queues
+using System.Diagnostics;                   //For clock, stopwatch
+using System.Globalization;                 //For regional specifics
+using System.Linq;                          //For lambda expressions
+using System.Numerics;                      //For BigInteger
+using System.Text;                          //For StringBuilder
+using System.Text.RegularExpressions;       //For Regex
+
+namespace E11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

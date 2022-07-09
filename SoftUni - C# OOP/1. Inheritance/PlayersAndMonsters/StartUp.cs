bf5e6cd970f041141978a256bd3cbd4c23785e21
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-09 11:33:20 +0300

    Added Players and Monsters

---
+++ SoftUni - C# OOP/1. Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Knight kn = new Knight("Rado", 19);
+            Console.WriteLine(kn);
+        }
+    }
+}

317fdbdd140df6a65b843c393ef9a408a98f317b
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-10 09:27:01 +0300

    Added Restaurant

---
+++ SoftUni - C# OOP/1. Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50m;
+        public double Caffeine { get; set; }
+        public Coffee(string name, double caffeine) : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public override string ToString()
+        {           
+            return $"{this.Name} - {this.Price} - {this.Milliliters} - {this.Caffeine}";
+        }
+
+    }
+}
---
+++ SoftUni - C# OOP/1. Inheritance/Restaurant/Fish.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Restaurant
+{
+    public class Fish : MainDish
+    {
+        public Fish(string name, decimal price) : base(name, price, fishGrams)
+        {
+        }
+
+        private const double fishGrams = 22; 
+    }
+}

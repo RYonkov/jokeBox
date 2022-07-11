8496b8691e9b1a19c48c4e4c1d47bf4890bdf0e7
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-11 06:50:03 +0300

    Added Zoo

---
+++ SoftUni - C# OOP/1. Inheritance/Zoo/Reptiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Reptiles : Animal
+    {
+        public Reptiles(string name) : base(name)
+        {
+
+        }
+    }
+}
---
+++ SoftUni - C# OOP/1. Inheritance/Zoo/Snake.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Snake : Reptiles
+    {
+        public Snake(string name) : base(name)
+        {
+        }
+    }
+}

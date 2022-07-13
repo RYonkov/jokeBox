3cf58a7ee5159a4e15cf76dace85f2604c9b7bc7
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-13 07:41:55 +0300

    Added Box

---
+++ SoftUni - C# OOP/2. Encapsulation/Box/Box.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        /// <summary>
+        /// Creates new instance with parameters lenght, width and height
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <summary>
+        /// Calculates the surface area of a box
+        /// </summary>
+        /// <returns></returns>
+        public double SurfaceArea()
+        {
+            return 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+        /// <summary>
+        /// Calculates the lateral surfice area of a box
+        /// </summary>
+        /// <returns></returns>
+        public double LateralSurfaceArea()
+        {
+            return 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+        /// <summary>
+        /// Calculates the volume of a box
+        /// </summary>
+        /// <returns></returns>
+        public double Volume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+    }
+}
---
+++ SoftUni - C# OOP/2. Encapsulation/Box/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box myBox = new Box(l, w, h);
+                Console.WriteLine($"Surface Area - {myBox.SurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {myBox.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {myBox.Volume():F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);               
+            }
+        }
+    }
+}

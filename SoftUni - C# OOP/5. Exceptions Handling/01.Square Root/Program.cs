042f03eb0e6b841d501d1b1ec940636918f93bcc
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-07-16 11:15:48 +0300

    Added 01. Square Root

---
+++ SoftUni - C# OOP/5. Exceptions Handling/01.Square Root/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.Square_Root
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double input = double.Parse(Console.ReadLine());
+            try
+            {
+                if (input<0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                
+                double output = Math.Sqrt(input);
+                Console.WriteLine(output);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}

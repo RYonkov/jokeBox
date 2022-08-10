5fd41a5d785454a352fd9bd68663e8b17e52b06f
Radoslav Yonkov <102038278+RYonkov@users.noreply.github.com>
2022-08-10 08:41:49 +0300

    Updated Raiding

--- SoftUni - C# OOP/4. Polymorphism/BaseHero/Paladin.cs
+++ SoftUni - C# OOP/4. Polymorphism/BaseHero/Paladin.cs
@@ -2,13 +2,13 @@
 {
     public class Paladin : BaseHero
     {
-        private const int DefaultPower = 100;
+        private const int DefaultPaladinPower = 100;
         public Paladin(string name, string type)
             : base(name, type)
         {
         }
 
-        public override int Power => DefaultPower;
+        public override int Power => DefaultPaladinPower;
 
         public override string CastAbility()
         {
